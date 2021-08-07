2bc422a6dcbdc60fbe69f8dd9e2cb25440231453
PIG208 <359101898@qq.com>
2021-08-07 10:13:39 +0800

    app: Setup router.

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,25 +1,24 @@
+import Campfire from './pages/campfire';
+import Home from './pages/home';
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 import './App.css';
 
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
+        <BrowserRouter>
+            <Switch>
+                <Route path='/home'>
+                    <Home />
+                </Route>
+                <Route path='/campfire'>
+                    <Campfire />
+                </Route>
+                <Route path='/'>
+                    <Redirect to='/home'/>
+                </Route>
+            </Switch>
+        </BrowserRouter>
     );
 }
 
