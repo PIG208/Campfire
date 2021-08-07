effe1b49ed60a15a577108f95d8215b7567ca804
PIG208 <359101898@qq.com>
2021-08-08 03:49:14 +0800

    campfire: Refactor campfile.

--- app/src/pages/campfire.tsx
+++ app/src/pages/campfire.tsx
@@ -1,57 +1,95 @@
+import Fire2 from '../components/fire-2';
 import React from 'react';
+import { Form, Input, notification } from 'antd';
+import { useHistory } from 'react-router-dom';
+import { Campfire, Service } from 'campfire-api';
 import { useState } from 'react';
 import '../css/campfire.css';
-import Fire2 from '../components/fire-2';
-import { Service } from 'campfire-api';
 
 export default function Topic() {
-    const [count, setCount] = useState(0);
-    const [name, setName] = useState("");
-    const [num, setNum] = useState(false);
+    const history = useHistory();
+    const [name, setName] = useState('');
+    const [campfire, setCampfire] = useState<Campfire | undefined>(undefined);
 
-    function getName(event: any) {
+    const handleSubmit = (data: { name: string }) => {
+        Service.postCampfire({
+            topic: data.name,
+        })
+            .then((result) => {
+                if (result.campfire) {
+                    setCampfire(result.campfire);
+                } else {
+                    return Promise.reject();
+                }
+            })
+            .catch((error) => {
+                notification.error({
+                    message: '失败',
+                    description: '未能创建篝火',
+                });
+                console.error(error);
+            });
+    };
+
+    function handleChange(event: any) {
         setName(event.target.value);
     }
 
     return (
         <>
             <div>
-                <div className="title2">
-                    <h1>发起你自己的篝火吧, 请为它取一个名字</h1>
-                    <input className="newfire" type="text" onChange={getName} />
-                </div>
-
-                <button
-                    className="create"
-                    onClick={() => {
-                        setCount((count) => 1);
-                        setNum((num) => true);
-                        Service.postCampfire({
-                            topic: name
-                        }).then((result)=>{
-                            setNum(result.result === 'success');
-                        });
-                    }}
+                <div
+                    className={`form-container ${
+                        campfire ? 'form-container-success' : ''
+                    }`}
                 >
-                    确认建立属于你的篝火
-                </button>
+                    <Form onFinish={handleSubmit} layout="vertical">
+                        <Form.Item
+                            label="发起你自己的篝火吧, 请为它取一个名字"
+                            name="name"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: '篝火名字不能为空',
+                                },
+                            ]}
+                        >
+                            <Input
+                                className="newfire"
+                                placeholder="你遇到的，以及他人可能遇到的困难"
+                                onChange={handleChange}
+                            />
+                        </Form.Item>
+                        <button className="create" type="submit">
+                            确认建立
+                            <br />
+                            属于你的篝火
+                        </button>
+                    </Form>
+                </div>
 
                 <a href="/home">
                     <button className="home">Home</button>
                 </a>
-
-                <div>
-                    {num ? (
-                        <div>
-                            <div className="msg">
-                                <h1>篝火已经成功建立了，已经有</h1>
-                                <h1>{count}</h1>
-                                <h1>人在emo </h1>
-                            </div>
-                        </div>
-                    ) : null}
-                    <Fire2 topic={name}/>
-                </div>
+                {campfire && (
+                    <button
+                        className="create"
+                        style={{
+                            left: '50%',
+                            transform: 'translateX(-50%)',
+                            top: '25vh',
+                            width: '50%',
+                            maxWidth: 200,
+                            position: 'absolute',
+                        }}
+                        onClick={() => {
+                            history.push(`/topic/${campfire.id}`);
+                        }}
+                    >
+                        {campfire.topic}
+                    </button>
+                )}
+                <Fire2 topic={name} />
             </div>
         </>
     );
