cc552282b2e0245bbe78e84b6a3282d1f7ba2487
PIG208 <359101898@qq.com>
2021-08-07 15:05:33 +0800

    topic: Display count.

--- app/src/pages/topic.tsx
+++ app/src/pages/topic.tsx
@@ -1,17 +1,29 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { useState } from 'react';
 import '../css/topic.css';
 
 export default function Topic() {
+    const [count, setCount] = useState(0);
+
+    /*
+    Service.postCampfireParticipate(1).then(result=>{
+
+    });*/
+
     return (
         <>
             <div>
                 <div className="title">
                     <h1>今天有</h1>
-                    <h1>xxx</h1>
+                    <h1>{count}</h1>
                     <h1>人和你一样在emo同一件事</h1>
                 </div>
 
-                <button className="add">加柴</button>
+                <button className="add" onClick={()=>{
+                    setCount(count=>count+1);
+                }}>
+                    加柴
+                </button>
                 <div className="container">
                     <div className="flame" id="flame-2"></div>
                     <div className="flame" id="flame-1"></div>
