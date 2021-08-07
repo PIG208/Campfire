2b8a6939a5c499f980ffbfabb8fa696d0180870d
PIG208 <359101898@qq.com>
2021-08-07 18:53:10 +0800

    landing: Improve the appearance of the landing page.

--- app/src/pages/home.tsx
+++ app/src/pages/home.tsx
@@ -1,6 +1,8 @@
+import { Button } from 'antd';
 import { Campfire, Service } from 'campfire-api';
 import { useState } from 'react';
 import { useEffect } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import Fire from '../components/fire';
 import '../css/landing.css';
 
@@ -13,6 +15,7 @@ window.onclick = () => {
 };
 
 export default function Home() {
+    const history = useHistory();
     const [topics, setTopics] = useState<Campfire[]>([]);
 
     useEffect(() => {
@@ -30,27 +33,38 @@ export default function Home() {
     }, []); // eslint-disable-line
 
     return (
-        <>
-            <div className="header">
-                <h1 className="name">Campfire 篝火计划</h1>
-                <a href="#" className="start">
-                    <h2>Start Your Own Fire 自己生火</h2>
-                </a>
+        <div className='landing'>
+            <div className="landing-header">
+                <h1 className="brand">Campfire 篝火计划</h1>
+                <Link to="/campfire" className="start">
+                    Start Your Own Fire 自己生火
+                </Link>
                 <h3 className="trending">Trending Emo 排行</h3>
             </div>
 
-            <button className="button random">Random 随机Emo</button>
-            <div className="topics">
-                {topics.map((val, index) => (
-                    <a key={index} href="#" className={`item-${index + 1}`}>
-                        {val.topic}
-                    </a>
-                ))}
-            </div>
+            <Button
+                className='btn-random'
+                onClick={() => {
+                    history.push('/topic');
+                }}
+            >
+                Random 随机Emo
+            </Button>
 
             <div className="mo-fire">
                 <Fire />
+                <div className="topics">
+                    {topics.map((val, index) => (
+                        <Link
+                            key={index}
+                            to={`/topic/${val.id}`}
+                            className={`floating-topic item-${index + 1}`}
+                        >
+                            {val.topic}
+                        </Link>
+                    ))}
+                </div>
             </div>
-        </>
+        </div>
     );
 }
