6bed523fca5c0fea4e627f62db57341daba690b7
hercysn <hercyshen@gmail.com>
2021-08-07 17:49:07 +0800

    fixed a bug in create a campfire

--- app/src/pages/campfire.tsx
+++ app/src/pages/campfire.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { ChangeEvent } from 'react';
 import { useState } from 'react';
 import '../css/topic.css';
 import '../css/campfire.css';
@@ -8,9 +8,9 @@ export default function Topic() {
     const [name, setName] = useState(null);
     const [num, setNum] = useState(false);
 
-    function getName(input)
+    function getName(event:any)
     {
-        setName(input.target.value)
+        setName(event.target.value)
     }
 
     return (
@@ -33,27 +33,27 @@ export default function Topic() {
                     num
                     ? 
                     <div>
-                    <div className="msg"> 
-                    <h1>篝火已经成功建立了， 有</h1>
-                    <h1>{count}</h1>
-                    <h1>人在emo</h1>
-                    <h1>{name}</h1>
+                        <div className="msg"> 
+                        <h1>篝火已经成功建立了， 有</h1>
+                        <h1>{count}</h1>
+                        <h1>人在emo </h1>
+                        <h1>{name}</h1>
                     </div>
                 
                 
                     <div className="container">
-                    <div className="flame" id="flame-2"></div>
-                    <div className="flame" id="flame-1"></div>
-                    <div className="flame" id="flame-3"></div>
-                    <div className="small-element" id="small-element-1"></div>
-                    <div className="small-element" id="small-element-2"></div>
-                    <div className="fire-bottom">
-                        <div className="main-fire"></div>
+                        <div className="flame" id="flame-2"></div>
+                        <div className="flame" id="flame-1"></div>
+                        <div className="flame" id="flame-3"></div>
+                        <div className="small-element" id="small-element-1"></div>
+                        <div className="small-element" id="small-element-2"></div>
+                        <div className="fire-bottom">
+                            <div className="main-fire"></div>
+                         </div>
+                        <div className="wood" id="wood-1"></div>
+                        <div className="wood" id="wood-2"></div>
+                        </div>
                     </div>
-                    <div className="wood" id="wood-1"></div>
-                    <div className="wood" id="wood-2"></div>
-                     </div>
-                     </div>
                     : null
                     }
                 </div>
