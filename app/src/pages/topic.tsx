5155bf527aefe7e08e68fb34423d61583ae975b3
PIG208 <359101898@qq.com>
2021-08-08 03:49:33 +0800

    topic: Enhance topic page.

--- app/src/pages/topic.tsx
+++ app/src/pages/topic.tsx
@@ -2,18 +2,21 @@ import React, { useCallback } from 'react';
 import { Button, Form, Input, Modal, notification } from 'antd';
 import { Campfire, Service } from 'campfire-api';
 import { useEffect } from 'react';
-import { useParams } from 'react-router';
+import { useHistory, useParams } from 'react-router';
 import { useState } from 'react';
 import '../css/topic.css';
 import Fire2 from '../components/fire-2';
 import SlidingBg from '../components/sliding-bg';
 
 export default function Topic() {
+    const history = useHistory();
     const [campfire, setCampfire] = useState<Campfire | undefined>();
     const [visible, setVisible] = useState(false);
     const [msgVisible, setMsgVisible] = useState(false);
-    const [comment, setComment] = useState("");
+    const [comment, setComment] = useState('');
     const id = parseInt(useParams<{ id: string }>().id);
+    const topic =
+        campfire && campfire.topic.length > 0 ? campfire.topic : '[无名]';
 
     const update = useCallback(() => {
         const random = id === undefined || isNaN(id);
@@ -21,10 +24,11 @@ export default function Topic() {
             (result) => {
                 if (result.campfires) {
                     setCampfire(result.campfires[0]);
+                    history.push(`/topic/${result.campfires[0].id}`);
                 }
             }
         );
-    }, [id]);
+    }, [id, history]);
 
     const handleSubmit = (data: any) => {
         setVisible(false);
@@ -34,14 +38,14 @@ export default function Topic() {
             id: target,
             content: data.content,
         }).then((result) => {
-            if(result.result === 'success') {
+            if (result.result === 'success') {
                 notification.success({
                     message: '成功',
                     description: '等待陌生人收到你的留言',
                 });
                 update();
             }
-        })
+        });
     };
 
     const handleAdd = () => {
@@ -49,12 +53,14 @@ export default function Topic() {
         if (isNaN(target) && campfire) target = campfire.id;
         Service.postCampfireParticipate(target).then((result) => {
             if (result.result === 'success') {
-                setVisible(true);
                 notification.success({
                     message: '成功',
                     description: '加柴成功',
                 });
                 update();
+                setTimeout(() => {
+                    setVisible(true);
+                }, 1500);
             }
         });
     };
@@ -63,15 +69,19 @@ export default function Topic() {
         let target = id;
         if (isNaN(target) && campfire) target = campfire.id;
         Service.getCampfireComment(target).then((result) => {
-            if(result.comment){
-                setMsgVisible(true);
+            if (result.comment) {
                 setComment(result.comment.content);
-            }
-            else {
-                
+                setTimeout(() => {
+                    setMsgVisible(true);
+                }, 500);
+            } else {
+                notification.error({
+                    message: '错误',
+                    description: '似乎还没有人在这里留下过痕迹...',
+                });
             }
         });
-    }
+    };
 
     useEffect(() => {
         update();
@@ -81,20 +91,24 @@ export default function Topic() {
         <>
             <div className="title">
                 <h1>今天有{campfire?.participants}人</h1>
-                <h1>和你一样在emo</h1>
+                <h1>和你一样在 emo</h1>
             </div>
-            <SlidingBg/>
-            <button className="add" onClick={handleAdd} style={{top: '60vh'}}>
+            <SlidingBg />
+            <button className="add" onClick={handleAdd} style={{ top: '60vh' }}>
                 加柴 Join in
             </button>
-            <button className="add" onClick={handlePick} style={{top: 'calc(60vh + 5rem)'}}>
-                抽取 {campfire?.topic} 下的留言
+            <button
+                className="add"
+                onClick={handlePick}
+                style={{ top: 'calc(60vh + 5rem)' }}
+            >
+                抽取 {topic} 下的留言
             </button>
             <a href="/home">
                 <button className="home">Home</button>
             </a>
 
-            <Fire2 topic={campfire?.topic}/>
+            <Fire2 topic={topic} />
 
             <Modal
                 visible={visible}
