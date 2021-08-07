eb629a8f1b8e31a73192bc592b07ef7ab2af9ab0
PIG208 <359101898@qq.com>
2021-08-08 00:05:46 +0800

    topic: Implement comment view.

--- app/src/pages/topic.tsx
+++ app/src/pages/topic.tsx
@@ -10,6 +10,8 @@ import Fire2 from '../components/fire-2';
 export default function Topic() {
     const [campfire, setCampfire] = useState<Campfire | undefined>();
     const [visible, setVisible] = useState(false);
+    const [msgVisible, setMsgVisible] = useState(false);
+    const [comment, setComment] = useState("");
     const id = parseInt(useParams<{ id: string }>().id);
 
     const update = useCallback(() => {
@@ -25,6 +27,20 @@ export default function Topic() {
 
     const handleSubmit = (data: any) => {
         setVisible(false);
+        let target = id;
+        if (isNaN(target) && campfire) target = campfire.id;
+        Service.postCampfireComment({
+            id: target,
+            content: data.content,
+        }).then((result) => {
+            if(result.result === 'success') {
+                notification.success({
+                    message: '成功',
+                    description: '等待陌生人收到你的留言',
+                });
+                update();
+            }
+        })
     };
 
     const handleAdd = () => {
@@ -42,6 +58,17 @@ export default function Topic() {
         });
     };
 
+    const handlePick = () => {
+        let target = id;
+        if (isNaN(target) && campfire) target = campfire.id;
+        Service.getCampfireComment(target).then((result) => {
+            setMsgVisible(true);
+            if(result.comment){
+                setComment(result.comment.content);
+            }
+        });
+    }
+
     useEffect(() => {
         update();
     }, [update]);
@@ -50,16 +77,19 @@ export default function Topic() {
         <>
             <div className="title">
                 <h1>今天有{campfire?.participants}人</h1>
-                <h1>和你一样在emo {campfire?.topic}</h1>
+                <h1>和你一样在emo</h1>
             </div>
-            <button className="add" onClick={handleAdd}>
+            <button className="add" onClick={handleAdd} style={{top: '60vh'}}>
                 加柴 Join in
+            </button>
+            <button className="add" onClick={handlePick} style={{top: 'calc(60vh + 5rem)'}}>
+                抽取 {campfire?.topic} 下的留言
             </button>
             <a href="/home">
                 <button className="home">Home</button>
             </a>
 
-            <Fire2 />
+            <Fire2 topic={campfire?.topic}/>
 
             <Modal
                 visible={visible}
@@ -81,6 +111,14 @@ export default function Topic() {
                     </Form.Item>
                     <Button htmlType="submit">留言 Submit</Button>
                 </Form>
+            </Modal>
+            <Modal
+                visible={msgVisible}
+                title="陌生人的留言"
+                onCancel={() => setMsgVisible(false)}
+                footer={null}
+            >
+                {comment}
             </Modal>
         </>
     );
